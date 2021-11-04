74ee43e5ecbba35bd5ca26270bf346ab43f1c492
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-04 13:05:18 +0100

    Part1 Confirm_Booking

--- DataAccess/Models/Booking.cs
+++ DataAccess/Models/Booking.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-namespace Booking
+namespace DataAccess.Model
 {
     public class Booking
     {
         public decimal TotalPrice { get; set; };
         public DateTime Date { get; set; };
 
-    public Booking (decimal TotalPrice, DateTime Date)
-    {
+        public Booking(decimal TotalPrice, DateTime Date)
+        {
             this.TotalPrice = TotalPrice;
             this.Date = Date;
-    }
-    
+        }
 
- }
+
+    }
+}
