49ce5826da9d74cf3fce1bc899494149b1ae4629
nikol2727 <n.yankova1314@gmail.com>
2021-11-08 14:21:56 +0100

    Establishing base for DataAccess for login user
    Added authentication Bcrypt static methods class for reusing hashing
    
    Added interfaces for user DataAccess
    
    Added user data access class
    
    Updating.csproj files

---
+++ DataAccess/DataAccess/UserDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using DataAccess.Interfaces;
+using DataAccess.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataAccess
+{
+    class UserDataAccess : BaseDataAccess<UserDataAccess>, IUserDataAccess
+    {
+        public UserDataAccess(string connectionstring) : base(connectionstring)
+        {
+        }
+
+        public async Task<int> LoginAsync(string email, string password)
+        {
+            try
+            {
+                var query = "SELECT id, password_hash_salt WHERE email=@Email";
+                using var connection = CreateConnection();
+                var userTuple = await connection.QuerySingleAsync<UserTuple>(query, new { Email = email });
+                if (BCryptTool.VerifyPassword(password, userTuple.Password_hash_salt))
+                {
+                    return userTuple.Id;
+                }
+                return -1;
+            }
+            catch(Exception ex)
+            {
+                //TODO: throw a more specific exception
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+    internal class UserTuple
+    {
+        public int Id;
+        public string Password_hash_salt;
+    }
+}
