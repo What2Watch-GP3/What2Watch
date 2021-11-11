1cf8a7fe0ccc2c11e1e423d2755b22a5860f25b7
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-11-11 14:27:14 +0100

    DtoConverter ~ By Miro

---
+++ WebApi/DTOs/BookingDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApi.DTOs
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ WebApi/DTOs/Converters/DtoConverter.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WebApi.DTOs.Converters
+{
+    public static class DtoConverter<T, U>
+    {
+        static MapperConfiguration config = new MapperConfiguration(cfg => cfg.CreateMap<T, U>());
+        static Mapper mapper = new Mapper(config);
+
+        //var bookingDto = DtoConverter<Booking, BookingDto>.From(booking);
+        //var booking = DtoConverter<BookingDto, Booking>.From(bookingDto);
+        public static T From(U to) => mapper.Map<U, T>(to);
+        public static IEnumerable<T> FromList(IEnumerable<U> to) => to.ToList().Select(obj => From(obj));
+    }
+} 
