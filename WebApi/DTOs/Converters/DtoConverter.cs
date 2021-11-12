cc59284694d605b01fb8edb68733262399f257f7
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-11-12 09:38:04 +0100

    DtoConverter

--- WebApi/DTOs/Converters/DtoConverter.cs
+++ WebApi/DTOs/Converters/DtoConverter.cs
@@ -6,9 +6,10 @@ namespace WebApi.DTOs.Converters
 {
     public static class DtoConverter<T, U>
     {
-        static MapperConfiguration config = new MapperConfiguration(cfg => cfg.CreateMap<T, U>());
+        static MapperConfiguration config = new MapperConfiguration(cfg => cfg.CreateMap<U, T>());
         static Mapper mapper = new Mapper(config);
 
+        //var bookingList = DtoConverter<Booking, BookingDto>.FromList(bookingDtoList);
         //var bookingDto = DtoConverter<Booking, BookingDto>.From(booking);
         //var booking = DtoConverter<BookingDto, Booking>.From(bookingDto);
         public static T From(U to) => mapper.Map<U, T>(to);
