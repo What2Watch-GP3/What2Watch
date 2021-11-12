f14dde32c4591327782dc8bc18064b1aa0dd512e
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-12 13:23:01 +0100

    TestWebApi

---
+++ TestWebApi/TestBookingWebApi.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using WebApi.DTOs;
+
+namespace TestBookingWebApi
+{
+    class TestBookingWebApi
+    {
+        private Booking _newBooking;
+        private BookingDto _newBookingDto;
+        private BookingController _testBookingWebApi;
+
+        [SetUp]
+        public async Task Setup()
+        {
+
+            _newBooking = new Booking() { TotalPrice = 23, Date = DateTime.Now };
+        }
+        //GETALL
+        [Test]
+        public async Task TestGetAllBookingsAsync()
+        {
+            //ARRANGE
+            //ACT
+            var bookings = await _testBookingWebApi.GetAllAsync();
+            //ASSERT
+            Assert.IsTrue(bookings.Value.Count() > 0, "Bookings weren't returned");
+        }
+
+        //GETBYID
+        [Test]
+        public async Task GetBookingByIdAsync()
+        {
+            //ARRANGE
+            //ACT
+            var booking = await _testBookingWebApi.GetByIdAsync(3);
+            //ASSERT
+            Assert.IsTrue(booking.Value.Equals(1) , "Bookings weren't returned");
+        }
+        //POST
+        [Test]
+        public async Task CreateBlogPostAsync()
+        {
+            //ARRANGE & ACT done in setup
+            var booking = await _testBookingWebApi.PostAsync(_newBookingDto);
+            //ASSERT
+            Assert.IsTrue(booking.Equals(0), "Bookings weren't created");
+        }
+
+        //DELETE
+        [Test]
+        public async Task DeleteBookingAsync()
+        {
+            //ARRANGE done in setup
+
+            //ACT
+            var booking = await _testBookingWebApi.DeleteAsync(1);
+            //ASSERT
+            Assert.IsTrue(booking.Equals(0), "Bookings weren't deleted");
+        }
+
+
+    }
+}
---
+++ WebApi/Controllers/BookingController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.DTOs;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+
+
+
+        // GET: api/<BookingController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookingDto>>> GetAllAsync()
+        {
+            return null;
+        }
+
+        // GET api/<BookingController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookingDto>> GetByIdAsync(int id)
+        {
+            return null;
+        }
+
+        // POST api/<BookingController>
+        [HttpPost]
+        public async Task<ActionResult<int>> PostAsync([FromBody] BookingDto value)
+        {
+            return null;
+        }
+
+        // PUT api/<BookingController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, [FromBody] BookingDto value)
+        {
+            return null;
+        }
+
+        // DELETE api/<BookingController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            return null;
+        }
+    }
+}
