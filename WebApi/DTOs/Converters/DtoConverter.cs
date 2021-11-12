c06eafa89c9258a3f203f9a4290cad98f5ba4bde
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-12 13:21:01 +0100

    TestWebApi

---
+++ WebApi/DTOs/Converters/DtoConverter.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WebApi.DTOs.Converters
+{
+    public static class DtoConverter<T, U>
+    {
+        private static MapperConfiguration config = new(cfg => cfg.CreateMap<U, T>());
+        private static Mapper mapper = new(config);
+
+        //var bookingList = DtoConverter<BookingDto, Booking>.FromList(bookingDtoList);
+        //var bookingDto = DtoConverter<Booking, BookingDto>.From(booking);
+        //var booking = DtoConverter<BookingDto, Booking>.From(bookingDto);
+        public static T From(U to) => mapper.Map<U, T>(to);
+        public static IEnumerable<T> FromList(IEnumerable<U> to) => to.ToList().Select(obj => From(obj));
+    }
+} 
