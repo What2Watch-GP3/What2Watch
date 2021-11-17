aac3e64946051a50958ae215543f834d6a21a056
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-17 13:01:28 +0100

    Revert "Revert "Confirm booking v2""

---
+++ DataAccess/Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<Ticket> Tickets { get; set; }
+
+    }
+}
