a33c5f31cbf4ebf51b386c2289eeeca9feb7375a
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-17 14:05:02 +0100

    webapi

---
+++ WebApiClient/DTOs/BookingDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiClient.DTOs
+{
+    class BookingDto
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
