c33df89cfd9a8d27909ff3add593e27deec400f4
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-17 14:06:43 +0100

    install restSharp package

--- WebApiClient/WhatToWatchApiClient.cs
+++ WebApiClient/WhatToWatchApiClient.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace WebApiClient
 {
-    class WhatToWatchWebApiClient : IWhatToWatchApiClient
+    class WhatToWatchApiClient : IWhatToWatchApiClient
     {
+
 
     }
 }
