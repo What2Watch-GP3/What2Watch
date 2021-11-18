e2a0969fe2578c718878ef07522e32bc65be6df6
Joan Romà Dot Codina <joandotcodina@users.noreply.github.com>
2021-11-18 09:19:28 +0100

    little commit

--- WebApiClient/WhatToWatchApiClient.cs
+++ WebApiClient/WhatToWatchApiClient.cs
@@ -9,7 +9,24 @@ namespace WebApiClient
 {
     class WhatToWatchApiClient : IWhatToWatchApiClient
     {
+        private RestClient _restClient;
+        public BlogSharpApiClient(string uri) => _restClient = new RestClient(new Uri(uri));
 
+        public async Task<int> CreateAuthorAsync(BookingDto entity)
+        {
+
+        }
+        public async Task<IEnumerable<BookingDto>> GetAllAuthorsAsync()
+        {
+
+
+
+        }
+
+        public async Task<AuthorDto> GetAuthorByIdAsync(int id)
+        {
+
+        }
 
     }
 }
