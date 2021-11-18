b698072516fe22cf9a83cc23d78b300302a1495a
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-11-18 09:29:33 +0100

    Constructor for the web api client

--- WebApiClient/WhatToWatchApiClient.cs
+++ WebApiClient/WhatToWatchApiClient.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RestSharp;
-
-namespace WebApiClient
-{
-    class WhatToWatchApiClient : IWhatToWatchApiClient
-    {
-
-
+﻿        private RestClient _restClient;
+        public WhatToWatchApiClient(string uri) => _restClient = new RestClient(new Uri(uri));
     }
 }
