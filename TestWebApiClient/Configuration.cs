fca1854841257c79c5f1593b584d2fd2332b5855
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-11-18 09:57:27 +0100

    creating general config class for web api url

---
+++ TestWebApiClient/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApiClient
+{
+    public static class Configuration
+    {
+        public const string WEB_API_URL = "https://localhost:5001/api/";
+    }
+}
