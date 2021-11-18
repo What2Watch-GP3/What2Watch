9b417aef5eba6b0c9fe6cebac99c9fe80035f7e9
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-11-18 13:32:53 +0100

    Implementing tests for WebApiClient for choose show user story - they also fucking work <3

--- WebApiClient/IWhatToWatchApiClient.cs
+++ WebApiClient/IWhatToWatchApiClient.cs
@@ -9,6 +9,11 @@ namespace WebApiClient
 {
     public interface IWhatToWatchApiClient
     {
+        Task<IEnumerable<MovieDto>> GetAllMoviesAsync();
+        Task<IEnumerable<CinemaDto>> GetCinemasByMovieIdAsync(int movieId);
+        Task<MovieDto> GetMovieByIdAsync(int id);
+        Task<IEnumerable<MovieDto>> GetMoviesByPartOfNameAsync(string searchString);
+        Task<IEnumerable<ShowDto>> GetShowsByMovieAndCinemaIdAsync(int movieId, int cinemaId);
         Task<int> CreateBookingAsync(BookingDto booking);
         Task<BookingDto> GetBookingByIdAsync(int id);
     }
