f4ade348f2b9beda6e71afa416ca785276a1f87c
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-11-22 13:41:30 +0100

    Desktop API Client + test

--- DesktopWebApiClient/DTOs/ShowDto.cs
+++ DesktopWebApiClient/DTOs/ShowDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebApiClient.DTOs
+namespace DesktopApiClient.DTOs
 {
     public class ShowDto
     {
---
+++ TestDesktopApiClient/ShowTest.cs
@@ -0,0 +1,34 @@
+﻿using DesktopApiClient;
+using DesktopApiClient.DTOs;
+using NUnit.Framework;
+using StubsClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDesktopApiClient
+{
+    class ShowTest
+    {
+        private IWhatToWatchApiClient _stubsClient;
+
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            _stubsClient = new WhatToWatchApiClient(new RestClientStub() { ResponseData = 1 });
+        }
+
+        [Test]
+        public async Task CreatedShowIdIsReturned()
+        {
+            //act
+            ShowDto show = new ShowDto() { Id = 1, StartTime = DateTime.Now };
+            var showId = await _stubsClient.CreateShowAsync(show);
+
+            //assert
+            Assert.AreEqual(1, showId, "Returned Show id was not the same");
+        }
+    }
+}
