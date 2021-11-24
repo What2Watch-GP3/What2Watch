87ef8cd01abaae2cf3a5cd842773c32427327d05
miroslavmarinov <1028757@ucn.dk>
2021-11-24 22:28:18 +0100

    Wrong connection string made tests failed - now fixed
    Somebody changed the tests to run on the dev DB. NOT OK. It’s fixed now

--- TestDataAccess/Configuration.cs
+++ TestDataAccess/Configuration.cs
@@ -8,7 +8,7 @@ namespace TestDataAccess
 {
     public static class Configuration
     {
-        public const string CONNECTION_STRING_TEST = "Data Source = hildur.ucn.dk; Initial Catalog = dmai0920_1086320; Persist Security Info=True;User ID = dmai0920_1086320; Password=Password1!";
+        public const string CONNECTION_STRING_TEST = "Data Source = hildur.ucn.dk; Initial Catalog = dmai0920_1028757; Persist Security Info=True;User ID = dmai0920_1028757; Password=Password1!";
 
     }
 }
