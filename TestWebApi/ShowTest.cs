4030539f2b898a095b81c6f5ee86b2e1120d5455
MaderK90 <1910653586@stud.fh-kufstein.ac.at>
2021-11-25 13:50:58 +0100

    fixed test issue

--- TestWebApi/ShowTest.cs
+++ TestWebApi/ShowTest.cs
@@ -45,21 +45,19 @@ namespace TestWebApi
         }
 
         [Test]
-        public async Task CreatingShowGettingTheRightStatusCode()
+        public async Task CreatingShowWithNullShowDtoGivesBadRequestResponse()
         {
             //arrange
             ShowDto show = null;
             //act
             var showResult = (await _showController.CreateShowAsync(show)).Result;
-            _objectResult = (ObjectResult)showResult;
-            int showId = (int)_objectResult.Value;
+            StatusCodeResult statusCodeResult = (StatusCodeResult)showResult;
             //assert
-            Assert.AreEqual(400, _objectResult.StatusCode, "Status coda was not BadRequest (400).");
-            Assert.IsTrue(showId > 0, $"Returned id was not more than 0. It was {showId}");
+            Assert.AreEqual(400, statusCodeResult.StatusCode, "Status coda was not BadRequest (400).");
         }
 
         [Test]
-        public async Task CreatingShowWithNullShowDtoGivesBadRequestResponse()
+        public async Task CreatingShowGettingTheRightStatusCode()
         {
             //arrange
             ShowDto show = new() { StartTime = DateTime.Now, MovieId = 1, RoomId = 1 };
