b58e35ecf00a6a5252933f4c4ae600ff20b33a29
MaderK90 <1910653586@stud.fh-kufstein.ac.at>
2021-12-06 13:45:33 +0100

    Finished WebApiClient

---
+++ TestWebApiClient/ReservationTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using StubsClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.DTOs;
+using WebApiClient;
+
+namespace TestWebApiClient
+{
+    class ReservationTest
+    {
+        private IWebApiClient _stubsClient;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _stubsClient = new WebApiClient.WebApiClient(new RestClientStub() { ResponseData = 1 });
+        }
+
+        [Test]
+        public async Task CreatingReservationReturnsCorrectId()
+        {
+            //Arrange
+            ReservationDto reservationDto = new ReservationDto() { TimeStamp = DateTime.Now, SeatId = 1, ShowId = 1, UserId = 1 };
+            //Act
+            int reservationId = await _stubsClient.CreateReservationAsync(reservationDto);
+            //Assert
+            Assert.IsTrue(reservationId > 0, $"Returned Id was wrong. Returned Id was {reservationId}.");
+        }
+    }
+}
