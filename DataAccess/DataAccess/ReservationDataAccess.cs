406f7416d72385999a35315ec437bc1b9094cbfa
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-12-09 13:16:50 +0100

    Refactored CreateAsync ReservationDataAcess
    Added a missing rollback for the transaction

--- DataAccess/DataAccess/ReservationDataAccess.cs
+++ DataAccess/DataAccess/ReservationDataAccess.cs
@@ -32,6 +32,7 @@ namespace DataAccess.DataAccess
                         bool isCreated = await connection.ExecuteAsync(command, reservation, transaction) > 0;
                         if(!isCreated)
                         {
+                            transaction.Rollback();
                             return false;
                         }
                     }
